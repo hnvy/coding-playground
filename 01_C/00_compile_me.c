cc4cefb1bb5bb9de8e7a69de05af9cbda86d8933
Harvey R <84394475+hnvy@users.noreply.github.com>
2022-10-24 00:29:58 +0100

    Update 00_compile_me.c

--- 01_C/00_compile_me.c
+++ 01_C/00_compile_me.c
@@ -1,26 +1,28 @@
-// TITLE: 01_intro.c
+// TITLE: 02_placeholders.c
 
-/*
-The following is what we call a dummy program. It literally does nothing:
-~main () {}~
+// We can use placeholders in order to "place" values in a string. Let's have a look:
 
-Let's print the good old "Hello, World!"
+#include <stdio.h> // We are going to use the ~printf~ function, so therefore we need to include the ~stdio.h~ header.
 
-~include~ is a called a pre-processor directive. This tells the compiler "What's up old friend, next time you compile this text file, mind including the contents of the stdio.h (aka standard input/output) file? Okay, cool. Bye!"
-
-The ~stdio.h~ is called a header file. It includes stuff (aka definitions) that we can use in our program. One of these definitions is... you guessed it, ~puts~.
-
-In fact, I realised that you can view the content of this ~stdio.h~ file.
-
-On Linux, it is located at ~/usr/include/stdio.h~.
-*/
-
-#include <stdio.h>
-
-int main() // This is called the entry point to the C program.
+int main()
 {
-	puts("Hello, World!");
+	printf("Hello, here we have a number in the string format (so, really, it's not a number): 73498\n"); // The ~\n~ is simply telling the program to start a new line (this is called an escape sequence). There is a whole massive list of these escape sequences: ~\t~ (for a tab), ~\\~ (for a literal slash character)...
+
+	printf("Woah, now we can use the placeholder print out an actual number: %d\n", 847562); // As can be seen, there is now a ~%d~ inside the double quotes. This is the placeholder. The number 847562 corresponds to that ~%d~.
+
+	printf("Let's experiment with some of these placeholders, shall we?\n");
+
+	printf("We can add many placeholders into one string, like so: (first number) %d - (second number) %d = (third number) %d\n", 10, 4, 6); // As can be seen, 10 corresponds to the first ~%d~, 4 corresponds to the second ~%d~, and finally, 6 corresponds to the third ~%d~. Cool, huh?
+
+	printf("Interestingly, we can make C do the maths for us! Like so: %d - %d = %d", 10, 4, 10-4); // The last argument (i.e., 10-4) makes C actually place the result of 10-4.
+
+	printf("\n\n\n"); // Printing 3 new lines to leave some space.
+	
+	printf("There is a\nnew line going\nto be inserted\nright here!\n\n");
+
+	printf("We can place some %s", "dinosaurs"); // You guessed it! ~%s~ is just like ~%d~, but instead, it's for strings rather than digits. You can replace "dinosaurs" with any word you like!
+
 	return(0);
 }
 
-// ** TEST 1: Test time! Write a program (from memory!) That prints out the following text: "This is my first ever mini C program. It's not much, but it's honest work..."
+// ** TEST 2: Test time! Write a program which prints out the following: There are many numbers in life, some arer big (like 297563476) and others are small (such as 3). Use the correct placeholder for the numbers.
