662d49b4b8762460b2bd0763a6e8cc7d507a0ea6
Harvey R <84394475+hnvy@users.noreply.github.com>
2022-10-23 16:49:24 +0100

    Update 03_variables.c

--- 01_C/03_variables.c
+++ 01_C/03_variables.c
@@ -7,7 +7,7 @@
 int main()
 {
 	// It seems that, usually, we should declare the variables before we create any statements.
-	int digit1, digit2, total; // ~int~ is a special keyword which C reads. It stands for "integer". You can replace the words which follow with whatever you like.
+	int digit, digital, total; // ~int~ is a special keyword which C reads. It stands for "integer". You can replace the words which follow with whatever you like.
 	float decimal; // ~float~ is another special word. It stands for floating-point. You can replace the word "decimal" with whatever you like.
 
 	/*
@@ -17,13 +17,12 @@ int main()
 
 	// Now, the stuff we declared above are all EMPTY! Let's give (i.e., assign) them some values. You must ALWAYS declare before assign.
 
-	digit1 = 30;
-	digit2 = 326;
+	digit = 30;
 	decimal = 23.827f; // Since this has a decimal point, it seems that people recommend that we add the letter ~f~ at the end.
 
-	total = digit1 * digit2; // Multiply the two digits.
+	total = digit1 * decimal; // Multiply the two digits.
 
-	printf("Multiplying %d and %d will give us %.3f", digit1, digit2, total);
+	printf("Multiplying %d and %d will give us %d", digit1, digit2, total);
 	// Ta-daaa! We used the placeholders (which we learned about in the 02_placeholders.c lesson) to create a beautiful ~printf~ statement!
 	// ^ Note how we have this ~.3f~? Well, this is to tell C that we want the answer to 3 decimal places.
 }
