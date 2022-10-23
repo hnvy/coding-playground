fb9e6cd3c67abaff1b437999f83a2198b7670245
Harvey R <84394475+hnvy@users.noreply.github.com>
2022-10-24 00:34:46 +0100

    Update 00_compile_me.c

--- 01_C/00_compile_me.c
+++ 01_C/00_compile_me.c
@@ -1,28 +1,24 @@
-// TITLE: 02_placeholders.c
-
-// We can use placeholders in order to "place" values in a string. Let's have a look:
-
-#include <stdio.h> // We are going to use the ~printf~ function, so therefore we need to include the ~stdio.h~ header.
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main()
 {
-	printf("Hello, here we have a number in the string format (so, really, it's not a number): 73498\n"); // The ~\n~ is simply telling the program to start a new line (this is called an escape sequence). There is a whole massive list of these escape sequences: ~\t~ (for a tab), ~\\~ (for a literal slash character)...
+    // fork() creates child process identical to parent
+    int pid = fork();
 
-	printf("Woah, now we can use the placeholder print out an actual number: %d\n", 847562); // As can be seen, there is now a ~%d~ inside the double quotes. This is the placeholder. The number 847562 corresponds to that ~%d~.
+    // if pid is greater than 0 than it is parent process
+    // if pid is 0 then it is child process
+    // if pid is -ve , it means fork() failed to create child process
 
-	printf("Let's experiment with some of these placeholders, shall we?\n");
+    // Parent process
+    if (pid > 0)
+        sleep(20);
 
-	printf("We can add many placeholders into one string, like so: (first number) %d - (second number) %d = (third number) %d\n", 10, 4, 6); // As can be seen, 10 corresponds to the first ~%d~, 4 corresponds to the second ~%d~, and finally, 6 corresponds to the third ~%d~. Cool, huh?
+    // Child process
+    else {
+        exit(0);
+    }
 
-	printf("Interestingly, we can make C do the maths for us! Like so: %d - %d = %d", 10, 4, 10-4); // The last argument (i.e., 10-4) makes C actually place the result of 10-4.
-
-	printf("\n\n\n"); // Printing 3 new lines to leave some space.
-	
-	printf("There is a\nnew line going\nto be inserted\nright here!\n\n");
-
-	printf("We can place some %s", "dinosaurs"); // You guessed it! ~%s~ is just like ~%d~, but instead, it's for strings rather than digits. You can replace "dinosaurs" with any word you like!
-
-	return(0);
+    return 0;
 }
-
-// ** TEST 2: Test time! Write a program which prints out the following: There are many numbers in life, some arer big (like 297563476) and others are small (such as 3). Use the correct placeholder for the numbers.
