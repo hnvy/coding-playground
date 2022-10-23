ae95919d805bd2c99562bf1a9b4e252ee9bf3703
Harvey R <84394475+hnvy@users.noreply.github.com>
2022-10-24 00:29:28 +0100

    Update 00_compile_me.c

--- 01_C/00_compile_me.c
+++ 01_C/00_compile_me.c
@@ -1,22 +1,26 @@
-#include <stdio.h> // Same old, same old...
+// TITLE: 01_intro.c
 
-int main()
+/*
+The following is what we call a dummy program. It literally does nothing:
+~main () {}~
+
+Let's print the good old "Hello, World!"
+
+~include~ is a called a pre-processor directive. This tells the compiler "What's up old friend, next time you compile this text file, mind including the contents of the stdio.h (aka standard input/output) file? Okay, cool. Bye!"
+
+The ~stdio.h~ is called a header file. It includes stuff (aka definitions) that we can use in our program. One of these definitions is... you guessed it, ~puts~.
+
+In fact, I realised that you can view the content of this ~stdio.h~ file.
+
+On Linux, it is located at ~/usr/include/stdio.h~.
+*/
+
+#include <stdio.h>
+
+int main() // This is called the entry point to the C program.
 {
-	/*
-	Sometimes, we might declare a variable, call it somewhere (e.g., in a ~printf~ statement), and get a weird error.
-
-	This might be because we have not assigned a value to that variable.
-
-	The issue outlined about can be easily overcome by initialising the variable with a value. Like so:
-
-	The following program will calculate the area of a rectangle...
-	*/
-
-	int width = 5, height = 10, area;
-
-	area = width * height;
-
-	printf("The area of the square is\n%d * %d = %d", height, width, area);
-
+	puts("Hello, World!");
 	return(0);
 }
+
+// ** TEST 1: Test time! Write a program (from memory!) That prints out the following text: "This is my first ever mini C program. It's not much, but it's honest work..."
