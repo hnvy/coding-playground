5f6c4674226db22c1472a924e80ce00abe472127
Harvey R <84394475+hnvy@users.noreply.github.com>
2022-10-24 00:38:48 +0100

    Update 00_compile_me.c

--- 01_C/00_compile_me.c
+++ 01_C/00_compile_me.c
@@ -1,24 +1,57 @@
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+ 
+/*
+Function : extractIpAddress
+Arguments : 
+1) sourceString - String pointer that contains ip address
+2) ipAddress - Target variable short type array pointer that will store ip address octets
+*/
+void extractIpAddress(unsigned char *sourceString,short *ipAddress)
+{
+    unsigned short len=0;
+    unsigned char  oct[4]={0},cnt=0,cnt1=0,i,buf[5];
+ 
+    len=strlen(sourceString);
+    for(i=0;i<len;i++)
+    {
+        if(sourceString[i]!='.'){
+            buf[cnt++] =sourceString[i];
+        }
+        if(sourceString[i]=='.' || i==len-1){
+            buf[cnt]='\0';
+            cnt=0;
+            oct[cnt1++]=atoi(buf);
+        }
+    }
+    ipAddress[0]=oct[0];
+    ipAddress[1]=oct[1];
+    ipAddress[2]=oct[2];
+    ipAddress[3]=oct[3];
+}
 
 int main()
 {
-    // fork() creates child process identical to parent
-    int pid = fork();
-
-    // if pid is greater than 0 than it is parent process
-    // if pid is 0 then it is child process
-    // if pid is -ve , it means fork() failed to create child process
-
-    // Parent process
-    if (pid > 0)
-        sleep(20);
-
-    // Child process
-    else {
-        exit(0);
-    }
-
+    unsigned char ip[20]={0};
+    short ipAddress[4];
+     
+    printf("Enter IP Address (xxx.xxx.xxx.xxx format): ");
+    scanf("%s",ip);
+     
+    extractIpAddress(ip,&ipAddress[0]);
+     
+    printf("\nIp Address: %03d. %03d. %03d. %03d\n",ipAddress[0],ipAddress[1],ipAddress[2],ipAddress[3]);
+    
+    if(ipAddress[0]>=0 && ipAddress[0]<=127)
+        printf("Class A Ip Address.\n");
+    if(ipAddress[0]>127 && ipAddress[0]<191)
+        printf("Class B Ip Address.\n");
+    if(ipAddress[0]>191 && ipAddress[0]<224)
+        printf("Class C Ip Address.\n");
+    if(ipAddress[0]>224 && ipAddress[0]<=239)
+        printf("Class D Ip Address.\n");
+    if(ipAddress[0]>239)
+        printf("Class E Ip Address.\n");
+     
     return 0;
 }
