5288b396afe6cfda6d824e0cc8ca993b40470c1b
Harvey R <84394475+hnvy@users.noreply.github.com>
2022-11-02 18:31:12 +0000

    Update feyn_it_right.py

--- 01_python/feyn_it_right.py
+++ 01_python/feyn_it_right.py
@@ -1,13 +1,11 @@
 import textstat
+import os
+import sys
 
-print ("When you are done with your explanation, write out '$$$' (no quotes) on a new line to tell the program that you are finished. It will then evaluate it and give you a school grade!")
-buffer = [] # This multiline idea was taken from a user called "Amber"
-while True: # Source: https://stackoverflow.com/questions/13128951/how-to-get-user-input-for-multiline-lines-in-python-3
-    print("> ", end="")
-    line = input()
-    if line == "$$$":
-        break
-    buffer.append(line)
-multiline_string = "\n".join(buffer)
-grade = textstat.text_standard(multiline_string)
+data_file = os.path.join(sys.path[0],'data.txt')
+
+with open(data_file, 'r') as buffer:
+    multiline_string = "\n".join(buffer)
+    grade = textstat.text_standard(multiline_string)
+
 print (grade)
