d10eb98f2a51e1c2ef18a2fe9f847ca0b61c4f8d
Harvey R <84394475+hnvy@users.noreply.github.com>
2023-08-02 15:31:51 +0100

    Update 00_compile_me.cc

--- 04_cc/00_compile_me.cc
+++ 04_cc/00_compile_me.cc
@@ -1,14 +1,26 @@
-#include <iostream>
+#include <SDL2/SDL.h>
 
-int main()
-{
-    int sum = 0, value = 0;
-    
-    while ((std::cin >> value))
-    {
-        sum += value;
-    }
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
 
-    std::cout << "Total value is " << sum << std::endl;
+    SDL_Window* window = SDL_CreateWindow("Hello, World!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Surface* surface = SDL_LoadBMP("hello.bmp");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(3000);
+
+    SDL_DestroyTexture(texture);
+    SDL_FreeSurface(surface);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
