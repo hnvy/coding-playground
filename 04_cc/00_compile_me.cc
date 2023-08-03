fd8d62409127ad7358ad05ab2e6db22007644fa5
Harvey R <84394475+hnvy@users.noreply.github.com>
2023-08-03 22:41:17 +0100

    Update 00_compile_me.cc

--- 04_cc/00_compile_me.cc
+++ 04_cc/00_compile_me.cc
@@ -1,35 +1,39 @@
 #include <SDL.h>
 #include <iostream>
 
-int main(int argv, char** args)
+int main( int argc, char* args[] )
 {
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
+    
+    //The surface contained by the window
+    SDL_Surface* screenSurface = NULL;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
-        std::cout << "Failed to initialize the SDL2 library\n";
-        return -1;
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
     }
-
-    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          680, 480,
-                                          0);
-
-    if(!window)
+else
     {
-        std::cout << "Failed to create window\n";
-        return -1;
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL )
+        {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+else
+        {
+            //Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Hack to get window to stay up
+            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+        }
     }
-
-    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
-
-    if(!window_surface)
-    {
-        std::cout << "Failed to get the surface from the window\n";
-        return -1;
-    }
-
-    SDL_UpdateWindowSurface(window);
-
-    SDL_Delay(5000);
-}
