eddf1426d50600427f32023848f0424ad3b10a68
Harvey R <84394475+hnvy@users.noreply.github.com>
2023-08-03 22:46:16 +0100

    Update 00_compile_me.cc

--- 04_cc/00_compile_me.cc
+++ 04_cc/00_compile_me.cc
@@ -1,6 +1,10 @@
 #include <SDL.h>
 #include <iostream>
 
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 int main(int argc, char * args[]) {
     //The window we'll be rendering to
     SDL_Window * window = NULL;
