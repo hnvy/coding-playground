ba12e0a7e3de793e73ca70117f672f54494d4927
Harvey R <84394475+hnvy@users.noreply.github.com>
2023-08-03 22:44:37 +0100

    Update 00_compile_me.cc

--- 04_cc/00_compile_me.cc
+++ 04_cc/00_compile_me.cc
@@ -1,39 +1,39 @@
 #include <SDL.h>
 #include <iostream>
 
-int main( int argc, char* args[] )
-{
+int main(int argc, char * args[]) {
     //The window we'll be rendering to
-    SDL_Window* window = NULL;
-    
+    SDL_Window * window = NULL;
+
     //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+    SDL_Surface * screenSurface = NULL;
 
     //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-    }
-else
-    {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        }
-else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+      printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    } else {
+      //Create window
+      window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+      if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+      } else {
+        //Get window surface
+        screenSurface = SDL_GetWindowSurface(window);
 
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
+        //Fill the surface white
+        SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface -> format, 0xFF, 0xFF, 0xFF));
 
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+        //Update the surface
+        SDL_UpdateWindowSurface(window);
+
+        //Hack to get window to stay up
+        SDL_Event e;
+        bool quit = false;
+        while (quit == false) {
+          while (SDL_PollEvent( & e)) {
+            if (e.type == SDL_QUIT) quit = true;
+          }
         }
+      }
     }
+}
