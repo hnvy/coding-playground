94580701de521fe1c1ad48db38d711f3484f5f34
Harvey R <84394475+hnvy@users.noreply.github.com>
2023-08-08 19:26:58 +0100

    Update 00_compile_me.cc

--- 04_cc/00_compile_me.cc
+++ 04_cc/00_compile_me.cc
@@ -1,33 +1,20 @@
 #include <iostream>
-#include <windows.h>
-#include <conio.h>
 
 int main()
 {
-    HWND hwnd = GetDesktopWindow();
-    HDC hdc = GetDC(hwnd);
-    int x = 100, y = 100; // Set the position of the text
-    bool blink = true;
+	int sum = 0, val = 0;
 
-    while (true)
-    {
-        if (blink)
-        {
-            TextOut(hdc, x, y, "Blinking text!", 14); // Display the text
-        }
-        else
-        {
-            Rectangle(hdc, x - 5, y - 5, x + 125, y + 25); // Erase the text using a rectangle
-        }
-        blink = !blink;
-        Sleep(500); // Wait for 500 milliseconds
-        if (_kbhit()) // Check if a key has been pressed
-        {
-            break; // Exit the loop if a key has been pressed
-        }
-    }
+	while (val <= 90)
+	{
+		sum += val;
+		++val;
+		std::cout << "The value is now " << val
+		<< std::endl;
+	}
 
-    ReleaseDC(hwnd, hdc);
-    return 0;
+	std::cout << "The total sum is " << sum
+	<< std::endl;
+
+	return 0;
 }
 
